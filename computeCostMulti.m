e8febae9a52b6e24c0bd9fc2db1e7f70b8e0bd9d
yaggya01 <54774316+yaggya01@users.noreply.github.com>
2020-06-06 00:06:50 +0530

    Add files via upload

---
+++ computeCostMulti.m
@@ -0,0 +1,12 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+
+m = length(y); % number of training examples
+
+J = 0;
+
+
+h_x = X*theta;    %compute h_theta(x) = theta^T * x
+J = sum((h_x - y) .^ 2)/2/m;   
+
+end
